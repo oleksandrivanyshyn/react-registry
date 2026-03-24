a89fc9f94cf6461334f5138462253c92c0dd2d17
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-24 14:19:14 +0200

    feat: add searchPackages API and PackageSummary type, integrate loader data into SearchPage

---
+++ src/api/queries/searchPackages.ts
@@ -0,0 +1,4 @@
+import type { PackageSummary } from '../types/packageSummary.ts';
+export async function searchPackages(
+  query: string,
+): Promise<PackageSummary[]> {}
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,8 @@
+import { useLoaderData } from 'react-router';
+
 const SearchPage = () => {
+  const data = useLoaderData();
+  console.log(data);
   return <div>Search Page</div>;
 };
 
