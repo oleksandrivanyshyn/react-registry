1620a6d4891b4b412bb774358ca3f0b00517dc4f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-24 16:49:31 +0200

    feat: integrate useLoaderData with DetailsPage and type detailsLoader result

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,5 +1,9 @@
+import { useLoaderData } from 'react-router';
+import type { DetailsLoaderResult } from './detailsLoader.ts';
+
 const DetailsPage = () => {
-  return <div>Details page</div>;
+  const { details } = useLoaderData() as DetailsLoaderResult;
+  return <div>{details.name}</div>;
 };
 
 export default DetailsPage;
--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,9 +1,16 @@
 import type { Params } from 'react-router';
 import { getPackage } from '../../api/queries/getPackage.ts';
+import type { PackageDetails } from '../../api/types/packageDetails.ts';
 interface LoaderArgs {
   params: Params;
 }
-export async function detailsLoader({ params }: LoaderArgs) {
+
+export interface DetailsLoaderResult {
+  details: PackageDetails;
+}
+export async function detailsLoader({
+  params,
+}: LoaderArgs): Promise<DetailsLoaderResult> {
   const { name } = params;
   if (!name) throw new Error('Package name is required');
   const details = await getPackage(name);
