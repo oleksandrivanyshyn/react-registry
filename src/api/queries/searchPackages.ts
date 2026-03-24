1d46f96e1bf87e0d856d234cebd0187973a1155b
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-24 14:26:28 +0200

    feat: implement searchPackages function to fetch and transform npm package data

--- src/api/queries/searchPackages.ts
+++ src/api/queries/searchPackages.ts
@@ -1,4 +1,20 @@
 import type { PackageSummary } from '../types/packageSummary.ts';
-export async function searchPackages(
-  query: string,
-): Promise<PackageSummary[]> {}
+interface SearchResponse {
+  objects: {
+    package: PackageSummary;
+  }[];
+}
+export async function searchPackages(term: string): Promise<PackageSummary[]> {
+  const res = await fetch(
+    `https://registry.npmjs.org/-/v1/search?text=${term}`,
+  );
+  const data: SearchResponse = await res.json();
+  return data.objects.map(
+    ({ package: { name, description, keywords, version } }) => ({
+      name,
+      description,
+      keywords,
+      version,
+    }),
+  );
+}
